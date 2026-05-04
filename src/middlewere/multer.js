0e3f58ad3fb2416d0fec5a2f5537030332d146ab
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-05 04:31:05 +0530

    error fix

--- src/middlewere/multer.js
+++ src/middlewere/multer.js
@@ -1,14 +1,16 @@
 import multer from "multer"
 import path from 'path'
 
-const storage  = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, "uploads/")
-    },
-    filename: function(req, file, cb){
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
+// const storage  = multer.diskStorage({
+//     destination: function(req, file, cb){
+//         cb(null, "uploads/")
+//     },
+//     filename: function(req, file, cb){
+//         cb(null, Date.now() + path.extname(file.originalname))
+//     }
+// })
+
+const storage = multer.memoryStorage();
 
 const upload = multer({storage});
 
